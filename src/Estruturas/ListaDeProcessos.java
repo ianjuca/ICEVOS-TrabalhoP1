ec2c6de313d219cdcb0b1782b9f69ce06c2b51df
ian jucá <ianrodriguesjuca@gmail.com>
2025-09-17 18:58:35 -0300

    corrigindo alguns erros
    corrigi uns erros que estavam na lista de processos

--- src/Estruturas/ListaDeProcessos.java
+++ src/Estruturas/ListaDeProcessos.java
@@ -1,84 +1,103 @@
 package Estruturas;
 
 public class ListaDeProcessos<Tipo> {
-    Node<Tipo> head,tail;
+    Node<Tipo> head, tail;
     public int tamanho;
 
-    public ListaDeProcessos(){
+    public ListaDeProcessos() {
         this.head = null;
         this.tail = null;
         this.tamanho = 0;
     }
 
-    public boolean add(Tipo valor, int pos){
-        if (pos < 0 ) || pos > tamanho) {
+    public boolean add(Tipo valor, int pos) {
+        if (pos < 0 || pos > tamanho) {
             return false;
         }
         Node<Tipo> novo = new Node<>(valor);
 
-        if (pos == 0){
-            if (this.head == null){
+        if (pos == 0) {
+            if (this.head == null) {
                 this.head = novo;
                 this.tail = novo;
-            }else{
-                Node<Tipo> atual = this.head;
-                for (int i= 0; i < pos - 1; i++){
-                    atual = atual.prox;
-                }
-                novo.prox = atual.prox;
-                novo.ant = atual;
-                atual.prox.ant = novo;
-                atual.prox = novo;
+            } else {
+                novo.prox = this.head;
+                this.head.ant = novo;
+                this.head = novo;
             }
-            tamanho++;
-            return true;
-        }
-        public void imprimir(){
+        } else if (pos == tamanho) {
+            novo.ant = this.tail;
+            this.tail.prox = novo;
+            this.tail = novo;
+        } else {
             Node<Tipo> atual = this.head;
-            Node<Tipo> atual2 = this.tail;
-            System.out.print("head -> ");
-
-            while (atual != null){
-                System.out.print(atual.valor + " -> ");
-                atual.prox;
+            for (int i = 0; i < pos - 1; i++) {
+                atual = atual.prox;
             }
-            System.out.print("null");
+            novo.prox = atual.prox;
+            novo.ant = atual;
+            atual.prox.ant = novo;
+            atual.prox = novo;
         }
+        tamanho++;
+        return true;
+    }
 
-        public Node<Tipo> remover(int pos){
-            if (pos < 0 || pos >= tamanho) {
-                return null;
-            }
-            Node<Tipo> removido;
-            if (pos == 0) {
-                removido = this.head;
-                if (tamanho == 1) {
-                    this.head = null;
-                    this.tail = null;
-                } else {
-                    Node<Tipo> atual = this.head;
-                    for (int i = 0; i < pos - 1; i++) {
-                        atual = atual.prox;
-                    }
-                    removido = atual.prox;
-                    atual.prox = removido.prox;
-                    removido.prox.ant = removido.ant;
-                    removido.ant = null;
-                    removido.prox = null;
-                }
-                tamanho--;
-                return removido;
-            }
-            public Tipo get ( int pos){
-                if (pos < 0 || pos >= tamanho) {
-                    return null;
-                }
-                Node<Tipo> atual = this.head;
-                for (int i = 0; i < pos; i++) {
-                    atual = atual.prox;
-                }
-                return atual.valor;
-            }
+    public void imprimir() {
+        Node<Tipo> atual = this.head;
+        System.out.print("head -> ");
+
+        while (atual != null) {
+            System.out.print(atual.valor + " -> ");
+            atual = atual.prox;
         }
+        System.out.print("null");
+    }
+
+    public Node<Tipo> remover(int pos) {
+        if (pos < 0 || pos >= tamanho) {
+            return null;
+        }
+        Node<Tipo> removido;
+        if (pos == 0) {
+            removido = this.head;
+            if (tamanho == 1) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.head = removido.prox;
+                removido.prox = null;
+                this.head.ant = null;
+            }
+        }else if(pos == tamanho - 1){
+            removido = this.tail;
+            this.tail = removido.ant;
+            this.tail.prox = null;
+            removido.ant = null;
+        }
+        else{
+            Node<Tipo> atual = this.head;
+            for (int i = 0; i < pos - 1; i++) {
+                atual = atual.prox;
+            }
+            removido = atual.prox;
+            atual.prox = removido.prox;
+            removido.prox.ant = removido.ant;
+            removido.ant = null;
+            removido.prox = null;
+        }
+        tamanho--;
+        return removido;
+    }
+
+    public Tipo get(int pos) {
+        if (pos < 0 || pos >= tamanho) {
+            return null;
+        }
+        Node<Tipo> atual = this.head;
+        for (int i = 0; i < pos; i++) {
+            atual = atual.prox;
+        }
+        return atual.valor;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import Componentes.Processo;
+import Componentes.Scheduler;
+import Estruturas.ListaDeProcessos;
+import LeitorTXT.Leitor;
 
+public class Main {
+    public static void main(String[] args){
+        Leitor leitor = new Leitor();
+        if (args.length < 1){
+            System.out.println("Uso: java Main <arquivo_de_processos.txt>");
+            System.exit(1);
+        }
+        ListaDeProcessos<Processo> lista_alta_prioridade = new ListaDeProcessos<Processo>();
+        ListaDeProcessos<Processo> lista_media_prioridade = new ListaDeProcessos<Processo>();
+        ListaDeProcessos<Processo> lista_baixa_prioridade = new ListaDeProcessos<Processo>();
+        String caminho = "processos.txt";
+        ListaDeProcessos<Processo> processos = leitor.carregarProcessos(caminho);
+        for (int i = 0; i < processos.tamanho; i++){
+            if (processos.get(i).getPrioridade() == 1){
+                lista_alta_prioridade.add(processos.get(i), lista_alta_prioridade.tamanho);
+            }
+        }
+    }
 }
